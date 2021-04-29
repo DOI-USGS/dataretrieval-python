e42dd0f424d983bfbdcf8ceb9f0e07e9903c662f
elmerehbi <elmerehbi@users.noreply.github.com>
2021-04-29 10:53:53 +0300

    allow list of stations // added docstrings

--- dataretrieval/flood_stage.py
+++ dataretrieval/flood_stage.py
@@ -1,19 +1,45 @@
-from typing import Dict
+from typing import Dict, List
 
 import requests
 
 ResponseFormat = "json" # json, xml
 
+# OLD web service dating to 2016; discontinued for new one?
 url = "https://waterwatch.usgs.gov/webservices/floodstage"
 
 
 def get_flood_stages(res_fmt: str = ResponseFormat) -> Dict:
+    """Retrieves flood stages for all stations."""
     res = requests.get(url, params={"format": res_fmt})
     if res.ok:
         stages = res.json()
         return {site['site_no']: {k: v for k, v in site.items() if k != 'site_no'} for site in stages['sites']}
 
 
-def get_flood_stage(site_no: str) -> Dict:
+def get_flood_stage(sites: List[str]) -> Dict[str, Dict]:
+    """
+    Retrieves flood stages for a list of station numbers.
+
+    Parameters
+    ----------
+    sites: List of strings
+        Site numbers
+
+    Returns
+    -------
+        Dictionary of station numbers and their flood stages. If no flood stage for a station None is returned.
+
+    Example
+    -------
+    >> stations = ["07144100", "07144101"]
+    >> print(get_flood_stage(stations))
+    {'07144100': {'action_stage': '20', 'flood_stage': '22', 'moderate_flood_stage': '25', 'major_flood_stage': '26'}, '07144101': None}
+    """
     stages = get_flood_stages()
-    return stages[site_no]
+    stations_stages = {}
+    for site in sites:
+        try:
+            stations_stages[site] = stages[site]
+        except KeyError:
+            stations_stages[site] = None
+    return stations_stages
