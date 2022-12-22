854ff7fa10fcd32c7c21eba38f64e081fbc1e12e
elbeejay <jhariharan@usgs.gov>
2022-12-22 11:59:01 -0500

    fix nadp url, improve doc-strings, add nadp tests

--- dataretrieval/nadp.py
+++ dataretrieval/nadp.py
@@ -4,21 +4,24 @@ the National Trends Network (NTN), the Mercury Deposition Network (MDN).
 
 National Trends Network
 -----------------------
-The  NTN provides longterm records of precipitation chemistry across the United States.
-See https://nadp.slh.wisc.edu/ntn for more info.
+The  NTN provides long-term records of precipitation chemistry across the
+United States. See https://nadp.slh.wisc.edu/ntn for more info.
 
 Mercury Deposition Network
 --------------------------
-The MDN provides longterm records of total mercury (Hg) concentration and deposition in precipitation in the United States and Canada.
-For more information visit https://nadp.slh.wisc.edu/networks/mercury-deposition-network/
+The MDN provides long-term records of total mercury (Hg) concentration and
+deposition in precipitation in the United States and Canada. For more
+information visit https://nadp.slh.wisc.edu/networks/mercury-deposition-network/
 
 Notes
 -----
-Gridded data on NADP is served as zipped tif files. Functions in this module will either download and extract the data,
-when a path is specified, or open the data as a GDAL memory-mapped file when no path is specified.
+Gridded data on NADP is served as zipped tif files. Functions in this module
+will either download and extract the data, when a path is specified, or open
+the data as a GDAL memory-mapped file when no path is specified.
 
 .. todo::
     - include AIRMoN, AMNet, and AMoN
+    - flexible handling of strings for parameters and measurement types
     - add errorchecking
     - add tests
 
@@ -38,18 +41,20 @@ from os.path import basename
 from uuid import uuid4
 
 NADP_URL = 'https://nadp.slh.wisc.edu'
-NADP_MAP_EXT = 'maplib/grids'
+NADP_MAP_EXT = 'filelib/maps'
 
-NTN_CONC_PARAMS = ['pH','So4','NO3','NH4','Ca','Mg','K','Na','Cl','Br']
-NTN_DEP_PARAMS  = ['H','So4','NO3','NH4','Ca','Mg','K','Na','Cl','Br','N','SPlusN']
+NTN_CONC_PARAMS = ['pH', 'So4', 'NO3', 'NH4', 'Ca',
+                   'Mg', 'K', 'Na', 'Cl', 'Br']
+NTN_DEP_PARAMS  = ['H', 'So4', 'NO3', 'NH4', 'Ca', 'Mg',
+                   'K', 'Na', 'Cl', 'Br', 'N', 'SPlusN']
 
-NTN_MEAS_TYPE = ['conc','dep','precip']  #concentration or deposition
+NTN_MEAS_TYPE = ['conc', 'dep', 'precip']  # concentration or deposition
 
 
 class GDALMemFile():
     """Creates a GDAL memory-mapped file
 
-    Modeled after `rasterio` function of same name
+    Modeled after ``rasterio`` function of same name
 
     .. code::
 
@@ -76,7 +81,7 @@ class GDALMemFile():
         see https://gist.github.com/jleinonen/5781308
 
         """
-        mmap_name = '/vsimem/' + uuid4().hex #vsimem is special GDAL string
+        mmap_name = '/vsimem/' + uuid4().hex  # vsimem is special GDAL string
         gdal.FileFromMemBuffer(mmap_name, self.buf)
 
         return gdal.Open(mmap_name)
@@ -86,31 +91,62 @@ class NADP_ZipFile(zipfile.ZipFile):
     """Extend zipfile.ZipFile for working on data from NADP
     """
     def tif_name(self):
+        """Get the name of the tif file in the zip file."""
         filenames = self.namelist()
         r = re.compile(".*tif$")
         tif_list = list(filter(r.match, filenames))
         return tif_list[0]
 
     def tif(self):
-        return self.read( self.tif_name() )
+        """Read the tif file in the zip file."""
+        return self.read(self.tif_name())
 
 
 def get_annual_MDN_map(measurement_type, year, path=None):
-    """Download a MDN map from NDAP
+    """Download a MDN map from NDAP.
+
+    This function looks for a zip file containing gridded information at:
+    https://nadp.slh.wisc.edu/maps-data/mdn-gradient-maps/.
+    The function will download the zip file and extract it, exposing the tif
+    file if a path is provided. If not, then a
+    :obj:`dataretrieval.nadp.GDALMemFile` object is returned.
 
     Parameters
     ----------
-    measurement_type : string
-        The type of measurement (concentration or deposition)
+    measurement_type: string
+        The type of measurement (concentration or deposition) as a string,
+        either 'conc' or 'dep' respectively.
 
-    year : string
+    year: string
+        Year as a string 'YYYY'
 
-    path : string
-        download directory
+    path: string
+        Download directory
+
+    Returns
+    -------
+    path: string
+        Path that zip file was extracted into if path was specified.
+
+    GDALMemFile: :obj:`dataretrieval.nadp.GDALMemFile`
+        GDALMemFile containing in-memory GDAL object.
+
+    Examples
+    --------
+    .. code::
+
+        >>> # get map of mercury concentration in 2010 and extract it to a path
+        >>> data_path = dataretrieval.nadp.get_annual_MDN_map(
+        ...     measurement_type='conc', year='2010', path='somepath')
+
+        >>> # get map of mercury deposition in 2008 as a GDALMemFile object
+        >>> gmem = dataretrieval.nadp.get_annual_MDN_map(
+        ...     measurement_type='dep', year='2008')
+
     """
-    url = '{}/{}/mdn/'.format(NADP_URL, NADP_MAP_EXT)
+    url = '{}/{}/MDN/grids/'.format(NADP_URL, NADP_MAP_EXT)
 
-    filename = 'Hg_{}_{}.zip'.format(measurement_type,year)
+    filename = 'Hg_{}_{}.zip'.format(measurement_type, year)
 
     z = get_zip(url, filename)
 
@@ -125,32 +161,52 @@ def get_annual_MDN_map(measurement_type, year, path=None):
 def get_annual_NTN_map(measurement_type, measurement=None, year=None, path=None):
     """Download a NTN map from NDAP.
 
+    This function looks for a zip file containing gridded information at:
+    https://nadp.slh.wisc.edu/maps-data/ntn-gradient-maps/.
+    The function will download the zip file and extract it, exposing the tif
+    file if a path is provided. If not, then a
+    :obj:`dataretrieval.nadp.GDALMemFile` object is returned.
+
+    .. note::
+
+        Measurement type abbreviations for concentration and deposition are
+        all lower-case, but for precipitation data, the first letter must be
+        capitalized!
+
     Parameters
     ----------
     measurement : string
         The measured constituent to return.
     measurement_type : string
-        The type of measurement (concentration, deposition, or precip)
+        The type of measurement, 'conc', 'dep', or 'Precip', which represent
+        concentration, deposition, or precipitation respectively.
     year : string
-
+        Year as a string 'YYYY'
     path : string
-        download directory
+        Download directory
 
     Returns
     -------
-    GDALMemFile containing in-memory GDAL object.
-    or
-    Path that data was extracted into if path was specified.
+    path: string
+        Path that zip file was extracted into if path was specified.
+
+    GDALMemFile: :obj:`dataretrieval.nadp.GDALMemFile`
+        GDALMemFile containing in-memory GDAL object.
 
     Examples
     --------
     .. code::
 
-        >>> dataretrieval.nadp.get_annual_NTN_map(
+        >>> # get a map of nitrate concentration in 1996
+        >>> gmem = dataretrieval.nadp.get_annual_NTN_map(
         ...     measurement='NO3', measurement_type='conc', year='1996')
 
+        >>> # get a map of precipitation in 2015 and extract it to a path
+        >>> data_path = dataretrieval.nadp.get_annual_NTN_map(
+        ...     measurement_type='Precip', year='2015', path='somepath')
+
     """
-    url = '{}/{}/{}/'.format(NADP_URL, NADP_MAP_EXT, year)
+    url = '{}/{}/NTN/grids/{}/'.format(NADP_URL, NADP_MAP_EXT, year)
 
     filename = '{}_{}.zip'.format(measurement_type, year)
 
@@ -170,10 +226,17 @@ def get_annual_NTN_map(measurement_type, measurement=None, year=None, path=None)
 def get_zip(url, filename):
     """Gets a ZipFile at url and returns it
 
+    Parameters
+    ----------
+    url : string
+        URL to zip file
+
+    filename : string
+        Name of zip file
+
     Returns
     -------
     ZipFile
-
 
     .. todo::
 
