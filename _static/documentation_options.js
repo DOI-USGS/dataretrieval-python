a26cdb08dfa907a9502488fe31e65f2be4a19676
thodson-usgs <34148978+thodson-usgs@users.noreply.github.com>
2023-02-15 17:12:39 +0000

    Deploying to gh-pages from @ USGS-python/dataretrieval@73f3ae8b4feacc3ab2bd8ea51e417232fd64fd6b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g54c8df5',
+    VERSION: '0.1.dev1+g73f3ae8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
