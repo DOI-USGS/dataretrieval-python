6474a249eb3939d1bfd2741a1724f87872811089
thodson <thodson@usgs.gov>
2024-06-03 20:12:24 -0500

    Update state codes

--- dataretrieval/codes/states.py
+++ dataretrieval/codes/states.py
@@ -1,54 +1,108 @@
 """List of 2-digit state codes with commented full names."""
-state_codes = [
-    'al',  # Alabama
-    'ak',  # Alaska
-    'az',  # Arizona
-    'ar',  # Arkansas
-    'ca',  # California
-    'co',  # Colorado
-    'ct',  # Connecticut
-    'de',  # Delaware
-    'dc',  # District of Columbia
-    'fl',  # Florida
-    'ga',  # Georgia
-    'hi',  # Hawaii
-    'id',  # Idaho
-    'il',  # Illinois
-    'in',  # Indiana
-    'ia',  # Iowa
-    'ks',  # Kansas
-    'ky',  # Kentucky
-    'la',  # Louisiana
-    'me',  # Maine
-    'md',  # Maryland
-    'ma',  # Massachusetts
-    'mi',  # Michigan
-    'mn',  # Minnesota
-    'ms',  # Mississippi
-    'mo',  # Missouri
-    'mt',  # Montana
-    'ne',  # Nebraska
-    'nv',  # Nevada
-    'nh',  # New Hampshire
-    'nj',  # New Jersey
-    'nm',  # New Mexico
-    'ny',  # New York
-    'nc',  # North Carolina
-    'nd',  # North Dakota
-    'oh',  # Ohio
-    'ok',  # Oklahoma
-    'or',  # Oregon
-    'pa',  # Pennsylvania
-    'ri',  # Rhode Island
-    'sc',  # South Carolina
-    'sd',  # South Dakota
-    'tn',  # Tennessee
-    'tx',  # Texas
-    'ut',  # Utah
-    'vt',  # Vermont
-    'va',  # Virginia
-    'wa',  # Washington
-    'wv',  # West Virginia
-    'wi',  # Wisconsin
-    'wy',  # Wyoming
-]
+state_codes = {
+    "Alabama":              "al",
+    "Alaska":               "ak",
+    "Arizona":              "az",
+    "Arkansas":             "ar",
+    "California":           "ca",
+    "Colorado":             "co",
+    "Connecticut":          "ct",
+    "Delaware":             "de",
+    "District of Columbia": "dc",
+    "Florida":              "fl",
+    "Georgia":              "ga",
+    "Hawaii":               "hi",
+    "Idaho":                "id",
+    "Illinois":             "il",
+    "Indiana":              "in",
+    "Iowa":                 "ia",
+    "Kansas":               "ks",
+    "Kentucky":             "ky",
+    "Louisiana":            "la",
+    "Maine":                "me",
+    "Maryland":             "md",
+    "Massachusetts":        "ma",
+    "Michigan":             "mi",
+    "Minnesota":            "mn",
+    "Mississippi":          "ms",
+    "Missouri":             "mo",
+    "Montana":              "mt",
+    "Nebraska":             "ne",
+    "Nevada":               "nv",
+    "New Hampshire":        "nh",
+    "New Jersey":           "nj",
+    "New Mexico":           "nm",
+    "New York":             "ny",
+    "North Carolina":       "nc",
+    "North Dakota":         "nd",
+    "Ohio":                 "oh",
+    "Oklahoma":             "ok",
+    "Oregon":               "or",
+    "Pennsylvania":         "pa",
+    "Rhode Island":         "ri",
+    "South Carolina":       "sc",
+    "South Dakota":         "sd",
+    "Tennessee":            "tn",
+    "Texas":                "tx",
+    "Utah":                 "ut",
+    "Vermont":              "vt",
+    "Virginia":             "va",
+    "Washington":           "wa",
+    "West Virginia":        "wv",
+    "Wisconsin":            "wi",
+    "Wyoming":              "wy",
+}
+
+fips_codes = {
+    "Alabama":              "01",
+    "Alaska":               "02",
+    "Arizona":              "04",
+    "Arkansas":             "05",
+    "California":           "06",
+    "Colorado":             "08",
+    "Connecticut":          "09",
+    "Delaware":             "10",
+    "District of Columbia": "11",
+    "Florida":              "12",
+    "Georgia":              "13",
+    "Hawaii":               "15",
+    "Idaho":                "16",
+    "Illinois":             "17",
+    "Indiana":              "18",
+    "Iowa":                 "19",
+    "Kansas":               "20",
+    "Kentucky":             "21",
+    "Louisiana":            "22",
+    "Maine":                "23",
+    "Maryland":             "24",
+    "Massachusetts":        "25",
+    "Michigan":             "26",
+    "Minnesota":            "27",
+    "Mississippi":          "28",
+    "Missouri":             "29",
+    "Montana":              "30",
+    "Nebraska":             "31",
+    "Nevada":               "32",
+    "New Hampshire":        "33",
+    "New Jersey":           "34",
+    "New Mexico":           "35",
+    "New York":             "36",
+    "North Carolina":       "37",
+    "North Dakota":         "38",
+    "Ohio":                 "39",
+    "Oklahoma":             "40",
+    "Oregon":               "41",
+    "Pennsylvania":         "42",
+    "Rhode Island":         "44",
+    "South Carolina":       "45",
+    "South Dakota":         "46",
+    "Tennessee":            "47",
+    "Texas":                "48",
+    "Utah":                 "49",
+    "Vermont":              "50",
+    "Virginia":             "51",
+    "Washington":           "53",
+    "West Virginia":        "54",
+    "Wisconsin":            "55",
+    "Wyoming":              "56",
+}
