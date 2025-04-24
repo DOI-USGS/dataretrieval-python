c9cbe679de7032ab41d6ef746e0b9a595cdf9108
Elise Hinman <ehinman@usgs.gov>
2025-04-24 11:56:53 -0500

    add more samples tests

--- tests/samples_test.py
+++ tests/samples_test.py
@@ -9,6 +9,7 @@ from dataretrieval.samples import (
 )
 
 def mock_request(requests_mock, request_url, file_path):
+    """Mock request code"""
     with open(file_path) as text:
         requests_mock.get(
             request_url, text=text.read(), headers={"mock_header": "value"}
@@ -45,6 +46,18 @@ def test_check_profiles():
     with pytest.raises(TypeError):
         _check_profiles(service="results", profile="foo")
 
+def test_samples_results():
+    """Test results call for proper columns"""
+    df,_ = get_USGS_samples(
+        service="results",
+        profile="narrow",
+        monitoringLocationIdentifier="USGS-05288705",
+        activityStartDateLower="2024-10-01",
+        activityStartDateUpper="2025-04-24"
+        )
+    assert all(col in df.columns for col in ["Location_Identifier", "Activity_ActivityIdentifier"])
+    assert len(df) > 0
+
 def test_samples_activity():
     """Test activity call for proper columns"""
     df,_ = get_USGS_samples(
@@ -55,3 +68,38 @@ def test_samples_activity():
     assert len(df) > 0
     assert len(df.columns) == 95
     assert "Location_HUCTwelveDigitCode" in df.columns
+
+def test_samples_locations():
+    """Test locations call for proper columns"""
+    df,_ = get_USGS_samples(
+        service="locations",
+        profile="site",
+        stateFips="US:55",
+        activityStartDateLower="2024-10-01",
+        activityStartDateUpper="2025-04-24",
+        usgsPCode="00010"
+        )
+    assert all(col in df.columns for col in ["Location_Identifier", "Location_Latitude"])
+    assert len(df) > 0
+
+def test_samples_projects():
+    """Test projects call for proper columns"""
+    df,_ = get_USGS_samples(
+        service="projects",
+        profile="project",
+        stateFips="US:15",
+        activityStartDateLower="2024-10-01",
+        activityStartDateUpper="2025-04-24"
+        )
+    assert all(col in df.columns for col in ["Org_Identifier", "Project_Identifier"])
+    assert len(df) > 0
+
+def test_samples_organizations():
+    """Test organizations call for proper columns"""
+    df,_ = get_USGS_samples(
+        service="organizations",
+        profile="count",
+        stateFips="US:01"
+        )
+    assert len(df) == 1
+    assert df.size == 3
